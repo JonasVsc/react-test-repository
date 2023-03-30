3ccaa9a37e428dc2f1c3d551e975e4564129a248
JonasVsc <jonasvasconcelosbarbosa@gmail.com>
2023-03-30 11:03:22 -0300

    Input generico cep e currency

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import Input from './components/Input'
 
-function App() {
+interface Usuario {
+  cep: string;
+  price: number;
+}
+
+const App: React.FC= () => {
+  const [usuario, setUsuario] = useState<Usuario>({} as Usuario);
+
+  const handleChange = useCallback((e: React.FormEvent<HTMLInputElement>) => {
+    setUsuario({
+      ...usuario,
+      [e.currentTarget.name]: e.currentTarget.value
+    })
+  }, [usuario]);
+
   return (
     <div>
-        teste
+      <Input placeholder='99999999' mask='cep' name='cep' onChange={handleChange}/>
+      <Input placeholder='' mask='currency' name='price' onChange={handleChange}/>
+
+      <button onClick={() => console.log(usuario)}>Salvar</button>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Input/index.tsx
@@ -0,0 +1,35 @@
+
+import React, { InputHTMLAttributes, useCallback } from "react";
+
+import { cep, currency } from './masks'
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+    mask: 'cep' | 'currency'
+    prefix?: string
+}
+
+
+const Input: React.FC<InputProps> = ({mask, prefix, ...props}) => {
+
+    const handleKeyUp = useCallback((e: React.FormEvent<HTMLInputElement> ) => {
+
+        if(mask === 'cep') {
+            cep(e)
+        }
+        if(mask === 'currency') {
+            currency(e)
+        }
+        
+
+    },[])
+
+    return (
+        <div>
+            {prefix && <span>{prefix}</span>}
+            <input {...props} onKeyUp={handleKeyUp}/>
+        </div>
+    )
+}
+
+
+export default Input
